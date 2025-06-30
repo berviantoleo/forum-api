61d8d608bb651ae3a1300cb8dddd90ca2358481f
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-30 19:23:57 +0700

    Update UserRepositoryPostgres.test.js

--- src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
@@ -21,7 +21,7 @@ describe('UserRepositoryPostgres', () => {
       const userRepositoryPostgres = new UserRepositoryPostgres(pool, {});
 
       // Action & Assert
-      await expect(userRepositoryPostgres.verifyAvailableUsername('dicoding')).rejects.toThrowError(InvariantError);
+      await expect(userRepositoryPostgres.verifyAvailableUsername('dicoding')).rejects.toThrow(InvariantError);
     });
 
     it('should not throw InvariantError when username available', async () => {
@@ -82,7 +82,7 @@ describe('UserRepositoryPostgres', () => {
       // Action & Assert
       return expect(userRepositoryPostgres.getPasswordByUsername('dicoding'))
           .rejects
-          .toThrowError(InvariantError);
+          .toThrow(InvariantError);
     });
 
     it('should return username password when user is found', async () => {
@@ -107,7 +107,7 @@ describe('UserRepositoryPostgres', () => {
       // Action & Assert
       await expect(userRepositoryPostgres.getIdByUsername('dicoding'))
           .rejects
-          .toThrowError(InvariantError);
+          .toThrow(InvariantError);
     });
 
     it('should return user id correctly', async () => {
