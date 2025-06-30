fb0cef1fb5116cb073205d532063a87833a3e22d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-30 19:17:41 +0700

    Update BcryptPasswordHash.test.js

--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -16,7 +16,7 @@ describe('BcryptEncryptionHelper', () => {
       expect(typeof encryptedPassword).toEqual('string');
       expect(encryptedPassword).not.toEqual('plain_password');
       expect(spyHash)
-          .toBeCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptEncryptionHelper
+          .toHaveBeenCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptEncryptionHelper
     });
   });
 
