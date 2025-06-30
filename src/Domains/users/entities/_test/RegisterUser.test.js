cb6adb9cc1d29d9642de5545c28088a9f891e2a4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-30 18:49:01 +0700

    Update RegisterUser.test.js

--- src/Domains/users/entities/_test/RegisterUser.test.js
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -10,7 +10,7 @@ describe('a RegisterUser entities', () => {
 
     // Action and Assert
     expect(() => new RegisterUser(payload)).
-        toThrowError('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+        toThrow('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
   });
 
   it('should throw error when payload did not meet data type specification',
@@ -37,7 +37,7 @@ describe('a RegisterUser entities', () => {
 
     // Action and Assert
     expect(() => new RegisterUser(payload)).
-        toThrowError('REGISTER_USER.USERNAME_LIMIT_CHAR');
+        toThrow('REGISTER_USER.USERNAME_LIMIT_CHAR');
   });
 
   it('should throw error when username contains restricted character', () => {
@@ -50,7 +50,7 @@ describe('a RegisterUser entities', () => {
 
     // Action and Assert
     expect(() => new RegisterUser(payload)).
-        toThrowError('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+        toThrow('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
   });
 
   it('should create registerUser object correctly', () => {
