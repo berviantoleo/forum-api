ea41e70e9ae2aab533ca298465bfc46989f341a9
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-06-30 18:52:41 +0700

    Update ClientError.test.js

--- src/Commons/exceptions/_test/ClientError.test.js
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -2,6 +2,6 @@ const ClientError = require('../ClientError');
 
 describe('ClientError', () => {
   it('should throw error when directly use it', () => {
-    expect(() => new ClientError('')).toThrowError('cannot instantiate abstract class');
+    expect(() => new ClientError('')).toThrow('cannot instantiate abstract class');
   });
 });
