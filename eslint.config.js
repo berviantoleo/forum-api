e053df2e51a9a4acb962f248a0ff638cc8785a6c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-09-08 20:04:52 +0700

    upgrade eslint to v9 and migrate to yarn

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        console: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly'
+      }
+    },
+    rules: {
+      'max-len': ['error', { code: 125, tabWidth: 2 }],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    }
+  },
+  {
+    ignores: ['docs/**/*.js']
+  }
+];
